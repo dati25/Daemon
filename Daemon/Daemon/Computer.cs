eb9d8dbc4def862255a1896c18501d099d8a1b09
Davit Shartava <shartavadavit0@gmail.com>
2023-03-19 16:19:25 +0100

    added basic stuff

---
+++ Daemon/Daemon/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daemon
+{
+    public class Computer
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string MacAddress { get; set; }
+        public string IpAddress { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- Daemon/Daemon/Program.cs
+++ Daemon/Daemon/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Daemon
+﻿using System.Net.Http.Json;
+
+namespace Daemon
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5105");
+
+            HttpResponseMessage message = await client.GetAsync("/api/Computer");
+            string data = await message.Content.ReadAsStringAsync();
+            Console.WriteLine(data);
+
+            Computer computer = new() { Name="Davit je gay", MacAddress="ae:31:de:fe:12", IpAddress="192.168.0.1", Status=false };
+            HttpResponseMessage msg = await client.PostAsJsonAsync<Computer>("/api/Computer", computer);
         }
     }
 }
